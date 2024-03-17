a537028d46e6c92ea20893ee0b4271d1024c34a6
devfilipealmeida <filipesilva504@gmail.com>
2024-03-17 15:55:52 -0300

    fix table name to partners

--- src/Entity/Partner.php
+++ src/Entity/Partner.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PartnerRepository::class)]
+#[ORM\Table(name: 'partners')]
 class Partner
 {
     #[ORM\Id]
