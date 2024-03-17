813d26a38279aa11c0ca4225b16d18844dacf11b
devfilipealmeida <filipesilva504@gmail.com>
2024-03-17 15:30:46 -0300

    finalizado crud de empresas

--- src/Controller/CorporationController.php
+++ src/Controller/CorporationController.php
@@ -18,7 +18,7 @@ class CorporationController extends AbstractController
     {
         return $this->json([
             'data' => $corporationRepository->findAll(),
-        ]);
+        ], 200);
     }
 
     #[Route('/corporations/{corporation}', name: 'corporation_by_id', methods: ['GET'])]
@@ -28,25 +28,26 @@ class CorporationController extends AbstractController
 
         if(!$corporation) return $this->json([
             'message' => 'Empresa não encontrada.'
-        ]);
+        ], 404);
 
         return $this->json([
             'data' => $corporation,
-        ]);
+        ], 200);
     }
 
     #[Route('/corporations/cnpj/{cnpj}', name: 'corporation_by_cnpj', methods: ['GET'])]
     public function getCorporationByCnpj(string $cnpj, CorporationRepository $corporationRepository): JsonResponse
     {
+        //busca a empresa pelo cnpj
         $corporation = $corporationRepository->findOneBy(['cnpj' => $cnpj]);
 
         if(!$corporation) return $this->json([
             'message' => 'Empresa não encontrada.'
-        ]);
+        ], 404);
 
         return $this->json([
             'data' => $corporation,
-        ]);
+        ], 200);
     }
 
     #[Route('/corporations', name: 'corporation_create', methods: ['POST'])]
@@ -59,7 +60,7 @@ class CorporationController extends AbstractController
             $data = $request->request->all();
         }
 
-        //verifica se já existe a empresa com o cnpj
+        //verifica se já existe a empresa com o cnpj informado
         $existingCorporation = $corporationRepository->findOneBy(['cnpj' => $data['cnpj']]);
         if($existingCorporation !== null) {
             return $this->json([
@@ -101,6 +102,76 @@ class CorporationController extends AbstractController
             'message' => 'Empresa cadastrada com sucesso.',
             'data' => $corporation,
         ], 201);
+    }
+
+    #[Route('/corporations/{corporation}', name: 'corporation_update', methods: ['PUT', 'PATCH'])]
+    public function update(int $corporation, Request $request, ManagerRegistry $doctrine, CorporationRepository $corporationRepository): JsonResponse
+    {
+        //verifica se a empresa existe
+        $corporation = $corporationRepository->find($corporation);
+        if(!$corporation) {
+            return $this->json([
+                'message' => 'Empresa não existe.'
+            ], 400);
+        }
+
+        //trata o formato da request, deixando o endpoint mais flexivel
+        if($request->headers->get('Content-Type') == 'application/json'){
+            $data = $request->toArray();
+        } else {
+            $data = $request->request->all();
+        }
+
+        //dados do responsável pela empresa/cadastro
+        $corporation->setResponsibleCompany($data['responsible_company']);
+        $corporation->setCpf($data['cpf']);
+        // Converte a string de data para um objeto DateTimeImmutable
+        $birthDate = DateTime::createFromFormat('d/m/Y', $data['birth_date']);
+        if ($birthDate === false) {
+            return $this->json([
+                'message' => 'Formato de data inválido. O formato correto é DD/MM/YYYY.'
+            ], 400);
+        }
+        $corporation->setBirthDate(new \DateTimeImmutable($birthDate->format('Y-m-d')));
+
+        //dados da empresa a ser cadastrada
+        $corporation->setFantasyName($data['fantasy_name']);
+        $corporation->setCnpj($data['cnpj']);
+        $corporation->setAddress($data['address']);
+        $corporation->setNeighborhood($data['neighborhood']);
+        $corporation->setComplement($data['complement']);
+        $corporation->setCity($data['city']);
+        $corporation->setState($data['state']);
+        
+        //data de cadastro e alterações da empresa
+        $corporation->setCreatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+        $corporation->setUpdatedAt(new \DateTimeImmutable('now', new \DateTimeZone('America/Sao_Paulo')));
+
+        $doctrine->getManager()->flush();
+
+        return $this->json([
+            'message' => 'Empresa atualizada com sucesso.',
+            'data' => $corporation,
+        ], 200);
+    }
+
+    #[Route('/corporations/{corporation}', name: 'corporation_delete', methods: ['DELETE'])]
+    public function delete(int $corporation, CorporationRepository $corporationRepository): JsonResponse
+    {
+        $corporation = $corporationRepository->find($corporation);
+
+        if(!$corporation) {
+            return $this->json([
+               'message' => 'Empresa não existe.'
+            ], 400);
+        }
+
+        $corporationRepository->remove($corporation, true);
+
+        return $this->json([
+            'message' => 'Empresa removida com sucesso',
+            'data' => $corporation
+        ], 200);
     }
 
 
