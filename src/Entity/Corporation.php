dcbb5b901fd3f084771907768fdfe93fd9a79a80
devfilipealmeida <filipesilva504@gmail.com>
2024-03-17 03:54:37 -0300

    created company entity and company repository

---
+++ src/Entity/Corporation.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CorporationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CorporationRepository::class)]
+class Corporation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $responsible_company = null;
+
+    #[ORM\Column(length: 11)]
+    private ?string $cpf = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $birth_date = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $fantasy_name = null;
+
+    #[ORM\Column(length: 14)]
+    private ?string $cnpj = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $neighborhood = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $complement = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $state = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getResponsibleCompany(): ?string
+    {
+        return $this->responsible_company;
+    }
+
+    public function setResponsibleCompany(string $responsible_company): static
+    {
+        $this->responsible_company = $responsible_company;
+
+        return $this;
+    }
+
+    public function getCpf(): ?string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): static
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeImmutable
+    {
+        return $this->birth_date;
+    }
+
+    public function setBirthDate(\DateTimeImmutable $birth_date): static
+    {
+        $this->birth_date = $birth_date;
+
+        return $this;
+    }
+
+    public function getFantasyName(): ?string
+    {
+        return $this->fantasy_name;
+    }
+
+    public function setFantasyName(string $fantasy_name): static
+    {
+        $this->fantasy_name = $fantasy_name;
+
+        return $this;
+    }
+
+    public function getCnpj(): ?string
+    {
+        return $this->cnpj;
+    }
+
+    public function setCnpj(string $cnpj): static
+    {
+        $this->cnpj = $cnpj;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): static
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getNeighborhood(): ?string
+    {
+        return $this->neighborhood;
+    }
+
+    public function setNeighborhood(string $neighborhood): static
+    {
+        $this->neighborhood = $neighborhood;
+
+        return $this;
+    }
+
+    public function getComplement(): ?string
+    {
+        return $this->complement;
+    }
+
+    public function setComplement(?string $complement): static
+    {
+        $this->complement = $complement;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): static
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+}
